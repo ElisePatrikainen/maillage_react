f5c8dd50212f762a7ae03835626bf940ad21fff8
patrikainen <patrikainen@osmos-group.com>
2020-03-21 10:57:53 +0100

    feature: front-page cards

--- src/front-page/FrontPageContent.js
+++ src/front-page/FrontPageContent.js
@@ -3,61 +3,72 @@ import './FrontPageContent.css';
 import Image1 from '../assets/images/front1bis.jpg';
 import Card from "../layout/Cards/Card";
 import cards_data from "../assets/data/frontPage";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faPlayCircle} from "@fortawesome/free-solid-svg-icons";
 
 
 const content = (
     <div>
 
-    <div id="section-1" className="fp-section">
-        <div className="with-large-image">
-            <img src={Image1}/>
-            <div className="text">
-                <div>Réseau de professionnels indépendants dédié au handicap psychique</div>
-                <div>Soin, aide, accompagnement : trouvez le professionnel adapté.</div>
+        <div id="section-1" className="fp-section">
+            <div className="with-large-image">
+                <img src={Image1}/>
+                <div className="text">
+                    <div className="title">
+                        <h1>1er réseau</h1><span> de professionnels indépendants dédié au </span>
+                        <h1>handicap </h1><h1>psychique </h1>
+                    </div>
+                    <div id="section-1-buttons-cont">
+
+                        <button className="dark"><FontAwesomeIcon className="icon" icon={faPlayCircle}/>Découvrir
+                        </button>
+                        <button className="clear"><FontAwesomeIcon className="icon" icon={faPlayCircle}/>Métiers
+                        </button>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
 
-    <div id="section-2" className="fp-section">
-        <div className="h1-cont">
-            <h1>Notre organisation</h1>
-        </div>
-        <div id="section-2-images">
-            <div className="cards">
-                {cards_data.map((detail) => (<Card data={detail} large={true}/>))}
+        <div id="section-2" className="fp-section">
+            <div className="h1-cont">
+                <h1>Notre organisation</h1>
             </div>
-        </div>
-        <button>En savoir plus</button>
-    </div>
-
-    <div id="section-3" className="fp-section">
-        <div className="h1-cont">
-            <h1>Notre organisation</h1>
-        </div>
-        <div className="with-large-image">
-            <img src={Image1}/>
-            <div className="text">
-                <div>Réseau de professionnels indépendants dédié au handicap psychique</div>
-                <div>Soin, aide, accompagnement : trouvez le professionnel adapté.</div>
+            <div id="section-2-images">
+                <div className="cards">
+                    {cards_data.map((detail) => (<Card data={detail} large={true}/>))}
+                </div>
             </div>
+            <button className="section-button">En savoir plus</button>
         </div>
-        <button>Bienvenue sur Maillage</button>
-    </div>
 
-    <footer>
-        <ul>
-            <li>Thérapie</li>
-            <li>Accompagnement social</li>
-            <li>Sport</li>
-            <li>Activités culturelles</li>
-        </ul>
-        <ul>
-            <li>Nos métiers</li>
-            <li>Notre charte</li>
-            <li>Qui sommes-nous ?</li>
-            <li>Associations partenaires</li>
-        </ul>
-    </footer>
+        <div id="section-3" className="fp-section">
+            <div className="h1-cont">
+                <h1>Notre organisation</h1>
+            </div>
+            <div className="with-large-image">
+                <img src={Image1}/>
+                <div className="text">
+                    <div>Réseau de professionnels indépendants dédié au handicap psychique</div>
+                    <div>Soin, aide, accompagnement : trouvez le professionnel adapté.</div>
+                </div>
+            </div>
+            <button className="section-button">Bienvenue sur Maillage</button>
+        </div>
+
+        <footer>
+            <ul>
+                <li>Thérapie</li>
+                <li>Accompagnement social</li>
+                <li>Sport</li>
+                <li>Activités culturelles</li>
+            </ul>
+            <ul>
+                <li>Nos métiers</li>
+                <li>Notre charte</li>
+                <li>Qui sommes-nous ?</li>
+                <li>Associations partenaires</li>
+            </ul>
+        </footer>
     </div>
 
 );
--- src/layout/Helpers/Helpers.js
+++ src/layout/Helpers/Helpers.js
@@ -4,7 +4,7 @@ import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import "./Helpers.css";
 
 function PageH2Before() {
-    return <span className="before-title"><FontAwesomeIcon icon={faFan}/></span>;
+    return <span className="before-title title-underlined"><FontAwesomeIcon icon={faFan}/></span>;
 }
 
 export {PageH2Before};
