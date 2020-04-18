d7cc76fd9db4d991c3844ffdbb68e71dd96f59f7
patrikainen <patrikainen@osmos-group.com>
2020-04-18 17:46:34 +0200

    texts

--- src/posts/Project.js
+++ src/posts/Project.js
@@ -10,18 +10,21 @@ function Project() {
 
                 <div className="section text">
                     <h1>Le projet</h1>
+                    <h2>
+                        <PageH2Before/>
+                        Un réseau de professionnels<span className="bold"> indépendants</span> :
+                    </h2>
                     <p>
                         Le projet de l’association MAILLAGE est né de multiples discussions avec différents
-                        professionnels du soin en cabinet libéral, au sujet de la prise en charges de personnes en
-                        situation de Handicap psychique, en ville.
-                        Nous avons partagé nos difficultés et nos réussites. Nous avons confronté notre expérience de la
-                        pratique clinique en ville et celle que certains avaient déjà au sein de différentes
-                        institutions.
-                        Une réflexion s’est engagée naturellement, non seulement sur les différents aspects de nos
-                        professions respectives mais aussi sur les difficultés que peuvent rencontrer les patients et
-                        leurs aidants à se repérer dans les offres de soin en ville. En effet celles-ci sont multiples,
-                        et logiquement peu liées entre elles, contrairement à ce qui est mis en place en institution.
-
+                        professionnels du soin en cabinet libéral, au sujet de la prise en charge des personnes en
+                        situation de Handicap psychique, en ville. Nous avons partagé nos difficultés et nos réussites.
+                        Nous avons confronté notre expérience de la pratique clinique en ville et celle que certains
+                        avaient déjà acquise au sein de différentes institutions. Une réflexion s’est engagée
+                        naturellement, non seulement sur les différents aspects de nos professions respectives, mais
+                        surtout sur les difficultés que peuvent rencontrer les patients et leurs aidants à se repérer
+                        dans les offres de soin en ville. En effet celles-ci sont multiples, parfois peu connues des
+                        personnes en demande, et logiquement peu liées entre elles, contrairement à ce qui est mis en
+                        place en institution.
                     </p>
                 </div>
 
@@ -31,56 +34,25 @@ function Project() {
                         Notre projet<span className="bold"> consiste</span> :
                     </h2>
                     <p>
-                        A présenter les différentes professions susceptibles d’être utiles à une prise en charge des
-                        difficultés rencontrées par les personnes en situation de handicap psychique.
-                        A regrouper des professionnels exerçant en ville, dont les différentes pratiques ont pour point
-                        commun d’être basées sur une écoute et une prise en compte bienveillante et sans jugement de ce
-                        qu’exprime le patient.
-                        A favoriser une réflexion riche de nos différentes pratiques professionnelles entre les membres
-                        de Maillage, au sujet de la prise en charge du handicap psychique en ville.
-                        A faciliter la possibilité d’un lien entre les différents professionnels qui, chacun dans son
-                        cadre respectif s’efforce de respecter les singularités d’un patient, de tenir compte de ses
-                        difficultés et s’appuyer sur ses ressources pour l’accompagner, à condition que celui-ci le
-                        souhaite et l’autorise. En effet, créer un lien de parole entre les différents professionnels
-                        autour d’un patient est un facteur de réussite dans certains cas particuliers.
-                        Soutenir l’effort vers le mieux- être des personnes ayant un handicap psychique, dans un
-                        contexte de vie en ville en alternative ou en complément de la prise en charge institutionnelle,
-                        est le fondement de notre projet.
-
+                        A offrir une présentation rapide de différentes professions susceptibles d’être utiles à une
+                        prise en charge au cas par cas, de personnes en situation de handicap psychique.
                     </p>
-                </div>
-
-                <div className="section text">
-                    <h2>
-                        <PageH2Before/>
-                        Un réseau de professionnels<span className="bold"> indépendants</span> :
-                    </h2>
                     <p>
-                        Le projet de l’association MAILLAGE est né de multiples discussions et réflexions entre
-                        différents professionnels du soin en cabinet libéral, au sujet de la prise en charges de
-                        personnes en situation de Handicap psychique, en ville.
-                        Nous avons partagé nos difficultés et nos réussites. Nous avons confronté notre expérience de la
-                        pratique clinique en ville et celle que certains avaient déjà au sein de différentes
-                        institutions.
-                        Le réseau permet aux patients et aux aidants de se repérer dans les différentes professions et
-                        de mieux connaitre les membres de l’association ayant signé la charte de Maillage.
+                        A faciliter le contact vers les professionnels indépendants exerçant en ville. Les membres de
+                        MAILLAGE sont tous garants de leur pratique et de leur orientation, mais celles ci ont pour
+                        point commun d’être basées sur une écoute et une prise en compte bienveillante et sans jugement
+                        de ce qu’exprime le patient, de s’efforcer de respecter les singularités d’un patient, de tenir
+                        compte de ses difficultés et s’appuyer sur ses ressources pour l’accompagner.
                     </p>
-                </div>
-
-                <div className="section colored-background text">
-                    <h2>
-                        <PageH2Before/>
-                        Une<span className="bold"> association</span> :
-                    </h2>
                     <p>
-                        L’association MAILLAGE favorise un cadre de rencontres trimestrielles pour permettre aux
-                        professionnels en ville et aux accompagnants de créer un lien de réflexion et de travail sur le
-                        Handicap psychique.
-                        Une réflexion s’engage ainsi naturellement, non seulement sur les différents aspects de nos
-                        professions respectives mais aussi sur les difficultés que peuvent rencontrer les patients et
-                        leurs aidants et les solutions susceptibles d’être mises en place. Différents axes de réflexion
-                        sont abordés, comme par exemple ; Comment améliorer l’offre de soin et d’accompagnement ?
-                        Comment offrir un minimum de contenance et de cohérence dans les prises en charge en ville ?
+                        L’objectif global de cette association de professionnels est de soutenir et accompagner l’effort
+                        vers le mieux- être des personnes ayant un handicap psychique, dans un contexte de vie et de
+                        consultation en ville, en alternative ou en complément de la prise en charge institutionnelle.
+                    </p>
+                    <p>
+                        Ce regroupement de professionnels favorise également une réflexion interdisciplinaire, riche de
+                        nos différentes pratiques professionnelles et de nos expériences diverses au sujet de la prise
+                        en charge du handicap psychique en ville.
                     </p>
                 </div>
 
